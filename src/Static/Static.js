e2e3533d9bb8526ff7c5750bea7c1f9a3b58cfd4
govind858 <govindjayakumar858@gmail.com>
2025-10-21 15:36:09 +0530

    changed to metrix server

--- src/Static/Static.js
+++ src/Static/Static.js
@@ -1,5 +1,5 @@
-const BaseURL = "https://neotokyo.pythonanywhere.com/"
-// const BaseURL = "https://metrix.pythonanywhere.com/"
+// const BaseURL = "https://neotokyo.pythonanywhere.com/"
+const BaseURL = "https://metrix.pythonanywhere.com/"
 
 
 export default BaseURL
